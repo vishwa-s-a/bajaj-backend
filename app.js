3c7f3918037008f65fa39f84a163608d2d75b11a
vishwa-s-a <vishwasappaji769@gmail.com>
2024-08-25 12:26:05 +0530

    adding cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); 
+
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
